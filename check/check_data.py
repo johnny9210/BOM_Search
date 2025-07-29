f1c71d865cb0569b1a44100a1be26df6ca2f0536
johnny9210 <jik9210@gmail.com>
2025-07-29 09:37:25 +0900

    check

---
+++ check/check_data.py
@@ -0,0 +1,157 @@
+tech_sections = {
+        "BM LIST": [
+            "TYPE(VALVE BODY TYPE)",
+            "CLASS(RATING), 압력급",
+            "MATERIAL, BODY MATERIAL",
+            "END CONNECTION(RF,FF, RTJ, SW, BW,NPT,WF)",
+            "BONNET TYPE ; (SEAL) WELD BONNET, BOLTED BONNET, PRESSURE SEAL BONNET, NON BONNET(BONNETLESS)",
+            "TRIM MATERIAL",
+            "DISC TYPE",
+            "LOCKING DEVICE(C.S.O, CSC)",
+            "WATER SEAL  CONNECTION",
+            "DESIGN PRESSURE, DESIGN TEMPRATURE",
+            "OPERATING PRESSURE, OPERATING TEMPRATURE",
+            "DIFFERENT PRESSURE(DELTA P)",
+            "SIZE",
+            "FORGING, CASTING",
+        ],
+        "PORT 규정": [
+            "BORE", 
+            "BORE DIAMETER",
+            "PORT", 
+            "PORT DIAMETER",
+            "STANDARD DIAMETER",
+            "ASME B16.34 APPENDIX.A",
+            "PIPE INSIDE DIAMETER",
+            "PORT CONNECTION(RF,FF, RTJ, SW, BW,NPT,WF)",
+        ],
+        "BONNET": [
+            "WELD BONNET",
+            "BOLTED BONNET",
+            "PRESSURE SEAL BONNET",
+            "NON BONNET(BONNETLESS)",
+        ],
+        "DISC(DISK) TYPE": [
+            "GATE V/V DISC TYPE - SOLID WEDGE",
+            "GATE V/V DISC TYPE - FLEXIBLE WEDGE",
+            "GATE V/V DISC TYPE - SPLIT WEDGE",
+            "GATE V/V DISC TYPE - DOUBLE",
+            "GATE V/V DISC TYPE - PARALLEL or PARALLEL SLIDE",
+            "GLOBE V/V DISC TYPE - PLUG",
+            "GLOBE V/V DISC TYPE - PARABOLIC",
+            "GLOBE V/V DISC TYPE - CONE",
+            "GLOBE V/V DISC TYPE - NEEDLE",
+            "GLOBE V/V DISC TYPE - NEEDLE POINT",
+            "GLOBE V/V DISC TYPE - LINNER",
+            "GLOBE V/V DISC TYPE - EQ %",
+            "GLOBE V/V DISC TYPE - QUICK OPEN",
+            "GLOBE V/V DISC TYPE - Y -TYPE",
+            "GLOBE V/V DISC TYPE - ANGLE",
+            "GLOBE V/V DISC TYPE - CONTINUOUS BLOW DOWN",
+            "GLOBE V/V DISC TYPE - INTERMIDIATE BLOW DOWN",
+            "GLOBE V/V DISC TYPE - FLOW CHARACTERISTIC",
+            "GLOBE V/V DISC TYPE - THROTTING SERVICE",
+            "GLOBE V/V DISC TYPE - DIFFERENT PRESSURE",
+            "CHECK V/V TYPE - SWING",
+            "CHECK V/V TYPE - LIFT",
+            "CHECK V/V TYPE - TILTING",
+            "CHECK V/V TYPE - NRV(NON RETURN VALVE)",
+            "CHECK V/V TYPE - NOZZLE",
+            "CHECK V/V TYPE - WAFER",
+            "CHECK V/V TYPE - STOP, Y-STOP",
+            "CHECK V/V TYPE - SPRING LOAD",
+            "CHECK V/V TYPE - PRESSUR DROP",
+            "CHECK V/V TYPE - INNER SHAFT",
+        ],
+        "SEAT 구조": [
+            "SEAT RING",
+            "RENEABLE SEAT",
+            "INTEGRAL SEAT",
+        ],
+        "MATERIAL": [
+            "BODY MATERIAL - FORGING, CASTING, ASTM ,ASME, JIS, KS, DIN, EN",
+            "TIRM MATERIAL( DISC, STEM SEAT) - STELLITE, HARDFACING, HARDENED",
+            "BOLTING MATERIAL - HOT DIP GALVANIZING", 
+            "BACKSEAT MATERIAL - STELLITE, HARDFACING, HARDENED", 
+        ],
+        "CONTRUCTION": [
+            "BY-PASS VALVE-THERMAL BINDING, MSS SP-61",
+            "EQUALIZING VALVE, BALANCE VALVE-OVER PRESSURE PROTECTION",
+            "DRAIN VALVE-SLUDGE, VENT",
+            "DISC HOLE(VENT HOLE)-OVER PRESSURE PROTECTION",
+            "SAFETY RELIEF DEVICE-OVER PRESSURE PROTECTION",
+            "POSITION INDICATOR-OPEN, CLOSE",
+            "STEM PROTECTOR-PHYSICAL DAMAGE",
+            "STEM COVER-PHYSICAL DAMAGE",
+            "LIVE LOADING SPRING-GRAND BOLT/NUT",
+            "VACUUM-BELLOW SEAL,DOUBLE PACKING, LANTERN RING, LEAK OFF, V TYPE TEPLON PACKING, VOC PACKING",
+            "LIMIT SWITCH-OPEN, CLOSE, INTERMIDIATE",
+            "LOCKING DEVICE(C.S.O, CSC)-RUST PREVENTION, STAINLESS",
+            "NON-RISING HANDWHEEL-NON ROTATING STEM",
+        ],
+        "OPERATING MODE": [
+            "GEAR OPERATED",
+            "MOV-RATED TORQUE, REQUIRED TORQUE 150%, 안전율 150%, 동작시간(OP/CL TIME)",
+            "POV(AOV)-공급 AIR 압력, 안전율 150%, 동작시간(OP/CL TIME)",
+        ],
+        "압력급": [
+            "SPECIAL CLASS, INTEMIDIATE"
+        ]
+    }
+
+QA_sections = {
+        "고객 구매주문서": [
+            "고객 구매주문서에서 요구하는 작업이 삼신의 인증서 범위 내에 있음"
+        ],
+        "밸브 형상": [
+            "밸브 형상(VALVE TYPE)"
+        ],
+        "성능 요건": [
+            "호칭관 경(Size)",
+            "ANSI 압력 등급(ANSI pressure class)"
+        ],
+        "다음 부품의 재료": [
+            "Body/Bonnet",
+            "Disk",
+            "Stem",
+            "Bolting",
+            "도료(Protective coating)",
+            "Gland packing",
+            "Gasket"
+        ],
+        "재료 요건": [
+            "열처리(Heat treatment)",
+            "충격시험(Impact test)"
+        ],
+        "용접 재료": [
+            "용접 재료(WELDING MATERIALS)"
+        ],
+        "밸브 악세사리": [
+            "밸브 악세사리(VALVE ACCESSORIES)"
+        ],
+        "세척 요건": [
+            "세척 요건(CLEANING)"
+        ],
+        "시험 요건": [
+            "수압 시험수 요건",
+            "기능(Functional)"
+        ],
+        "표시, 포장 및 출하": [
+            "표시, 포장 및 출하(MARKING, PACKAGING, AND SHIPPING)"
+        ],
+        "검사": [
+            "검사(INSPECTION)"
+        ],
+        "비파괴 검사요건": [
+            "비파괴 검사요건(NDE REQUIREMENTS)"
+        ],
+        "제출 문서": [
+            "제출 문서(DOCUMENT TO BE PROVIDED)"
+        ],
+        "품질보증기록": [
+            "품질보증기록(QA RECORDS)"
+        ],
+        "납기": [
+            "납기(DELIVERY)"
+        ]
+    }
